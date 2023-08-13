b0f12044438039f7fb9fc96e530fcd693e3c31aa
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-08-13 12:18:54 +0100

    feat: initial form page styled for desktop

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -29,58 +29,61 @@ const Form: React.FC = () => {
   };
 
   return (
-    <div className="container-fluid bg-white px-12 py-16 rounded-[2rem]">
-      {!isSubmitted ? (
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          <h2 className="text-[3rem] font-bold mb-4 text-indigo-900">
-            Stay updated!
-          </h2>
-          <p className="mb-4 text-gray-600">
-            Join 60,000+ product managers receiving monthly updates on:
-          </p>
-          <ul className="mb-4">
-            <li className="flex items-center mb-2 ">
-              <img className="mr-4" src={listTicks} alt="list" />
-              <p className="text-gray-600">
-                Product discovery and building what matters
-              </p>
-            </li>
-            <li className="flex items-center mb-2">
-              <img className="mr-4" src={listTicks} alt="list" />
-              <p className="text-gray-600">
-                Measuring to ensure updates are a success
-              </p>
-            </li>
-            <li className="flex items-center mb-2">
-              <img className="mr-4" src={listTicks} alt="list" />
-              <p className="text-gray-600">And much more!</p>
-            </li>
-          </ul>
-          <label className="mb-2 text-indigo-900">Email address</label>
-          <input
-            className={` ${
-              error && "border-red-500"
-            } w-full p-3 mb-4 border-2 border-gray-300 rounded-md`}
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="email@company.com"
-          />
+    <div className="flex items-center justify-center container-fluid bg-white pr-7 pl-12 pt-6 pb-6 rounded-[2rem]">
+      <div className="mr-8">
+        {!isSubmitted ? (
+          <form onSubmit={handleSubmit} className="flex flex-col">
+            <h2 className="text-[3rem] font-bold mb-4 text-indigo-900">
+              Stay updated!
+            </h2>
+            <p className="mb-4 text-gray-600 max-w-sm">
+              Join 60,000+ product managers receiving monthly updates on:
+            </p>
+            <ul className="mb-4">
+              <li className="flex items-center mb-2 ">
+                <img className="mr-4" src={listTicks} alt="list" />
+                <p className="text-gray-600">
+                  Product discovery and building what matters
+                </p>
+              </li>
+              <li className="flex items-center mb-2">
+                <img className="mr-4" src={listTicks} alt="list" />
+                <p className="text-gray-600">
+                  Measuring to ensure updates are a success
+                </p>
+              </li>
+              <li className="flex items-center mb-2">
+                <img className="mr-4" src={listTicks} alt="list" />
+                <p className="text-gray-600">And much more!</p>
+              </li>
+            </ul>
+            <label className="mb-2 text-indigo-900">Email address</label>
+            <input
+              className={` ${
+                error && "border-red-500"
+              } w-full p-3 mb-4 border-2 border-gray-300 rounded-md`}
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="email@company.com"
+            />
 
-          <button
-            className="w-full p-3 text-white bg-indigo-900 rounded-md font-medium"
-            type="submit"
-          >
-            Subscribe to monthly newsletter
-          </button>
-          {error && <p>Invalid email</p>}
-        </form>
-      ) : (
-        <form onSubmit={handleDismiss}>
-          <p>Thanks for subscribing!</p>
-          <button type="submit">Dismiss message</button>
-        </form>
-      )}
+            <button
+              className="w-full p-3 text-white bg-indigo-900 rounded-md font-medium"
+              type="submit"
+            >
+              Subscribe to monthly newsletter
+            </button>
+            {error && <p>Invalid email</p>}
+          </form>
+        ) : (
+          <form onSubmit={handleDismiss}>
+            <p>Thanks for subscribing!</p>
+            <button type="submit">Dismiss message</button>
+          </form>
+        )}
+      </div>
+      <div className="bg-desktop py-[16rem] px-[11rem] rounded-xl"></div>
     </div>
   );
 };
